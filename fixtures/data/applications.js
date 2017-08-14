1d450f31713c0c25b448faa9d71db29ffa52782c
sebastien-cas <sebastien.cas@isics.fr>
2017-08-14 09:13:15 +0200

    updated fixtures according to documentation

--- fixtures/data/applications.js
+++
@@ -1,5 +0,0 @@
-exports.applications = [
-    { name: 'App1', ref: 'app1' },
-    { name: 'App2', ref: 'app2' },
-    { name: 'App3', ref: 'app3' }
-];
---
+++ fixtures/data/candidate.js
@@ -0,0 +1,5 @@
+exports.candidates = [
+    { name: 'Candidate #1', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus sequi asperiores voluptate inventore culpa dolores nostrum consequuntur autem. Perspiciatis dolor nisi alias dolore autem! A sequi consequuntur eveniet sapiente accusamus.' },
+    { name: 'Candidate #2', description: 'Donec finibus, magna a semper dapibus, nisi nibh suscipit enim, quis pharetra nisi mauris a ligula.' },
+    { name: 'Candidate #3', description: 'Morbi rhoncus pretium vulputate. In hac habitasse platea dictumst. Proin mattis consectetur nunc, ac hendrerit quam lobortis ac. Praesent eget felis eu dolor euismod tempor. Fusce in aliquet erat.' }
+];
