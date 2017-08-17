d39f0034a5f5dc2fd83875a0a5632df5915bfcc4
sebastien-cas <sebastien.cas@isics.fr>
2017-08-17 22:31:28 +0200

    added domain blacklist for email validation

--- models/vote.js
+++ models/vote.js
@@ -46,11 +46,21 @@ voteSchema.path('email').validate(function() {
 voteSchema.path('email').validate(function(value) {
     return new Promise(function(resolve) {
         const config = require('../config');
-        var isemail = require('isemail');
+        var isValid = require('isemail').validate(value, config['emails']['validator_options']);
+        if (!isValid) {
+            resolve(false);
+        }
 
-        resolve(isemail.validate(value, config['emails']['validator_options'], function(result) {
-            //console.log(result);
-        }));
+        const lodash = require('lodash');
+        var parts = value.split('@');
+        var domains = parts[1].split('.');
+        lodash.forEach(domains, function(domain) {
+            if (lodash.includes(config['domain_blacklist'], domain)) {
+                resolve(false);
+            }
+        });
+
+        resolve(true);
     });
 
 }, 'Email address is invalid');
